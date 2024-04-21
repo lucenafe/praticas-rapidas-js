da0bf105d1cf175b8007b569094aa1ca8b4166bc
lucenafe <desenvolvedor.lucena@gmail.com>
2024-04-21 11:44:54 -0300

    adicionar - pratica-03.js

--- pratica-03.js
+++ pratica-03.js
@@ -1,3 +1,12 @@
 // Considere uma situação em que você está verificando se um usuário está logado e tem permissão de administrador para acessar determinada funcionalidade. Utilize variáveis booleanas para simular essas condições e use o operador AND para verificar se ambas são verdadeiras.
 
 var statusLogin = true;
+var acessoAdministrador = false;
+
+if (statusLogin && acessoAdministrador) {
+    var permissoes = 'O usuário está logado e possui permissões de edição';
+} else {
+    var permissoes = 'O usuário não está logado ou não possuí permissões de edição';
+}
+
+console.log(permissoes)
