7c9157fec95fbb9cc27718c47a187a76709a18b4
lucenafe <desenvolvedor.lucena@gmail.com>
2024-04-21 11:30:17 -0300

    adicionar - pratica-02.js

---
+++ pratica-02.js
@@ -0,0 +1,5 @@
+// Crie uma variável numérica com um valor. Utilize um operador ternário para verificar se esse valor é par ou ímpar. Exiba o resultado no console.
+
+var varNumerica = 551
+
+console.log(`${parOuImpar = ( varNumerica % 2 === 0) ? "par" : "ímpar"}`);
